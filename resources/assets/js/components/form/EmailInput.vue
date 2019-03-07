9afde82c39a9c385034361bcf13355a869274dda
SlavikTZ <vyacheslavlysen@gmail.com>
2019-03-07 19:00:48 +0200

    change template pug

--- resources/assets/js/components/form/EmailInput.vue
+++ resources/assets/js/components/form/EmailInput.vue
@@ -1,26 +1,25 @@
-<template>
-  <div>
-    <v-text-field
-      browser-autocomplete="current-password"
-      :class="errorClass"
-      :counter="counter"
-      :error-messages="errorMessages"
-      :hint="hint"
-      :label="label"
-      :name="name"
-      :prepend-icon="prepend"
-      type="email"
-      v-model="_value"
-    ></v-text-field>
-    <has-error :form="form" :field="name"></has-error> 
-  </div>
+<template lang="pug">
+    div
+        v-text-field(browser-autocomplete="current-password",
+        :class="errorClass",
+        :counter="counter",
+        :error-messages="errorMessages",
+        :hint="hint",
+        :label="label",
+        :name="name",
+        :prepend-icon="prepend",
+        type="email",
+        v-model="_value"
+        )
+        has-error(:form="form", :field="name")
+
 </template>
 
 <script>
-import TextInput from './TextInput'
+    import TextInput from './TextInput'
 
-export default {
-  extends: TextInput,
-  name: 'email-input'
-}
+    export default {
+        extends: TextInput,
+        name: 'email-input'
+    }
 </script>
